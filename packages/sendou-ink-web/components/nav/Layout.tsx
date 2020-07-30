fd84b0fe5fd743540c62eab3d19ac6f8ceaf089e
Sendou <38327916+Sendouc@users.noreply.github.com>
2020-07-30 20:55:34 +0300

    layout paddings

--- packages/sendou-ink-web/components/nav/Layout.tsx
+++ packages/sendou-ink-web/components/nav/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TopNav from "./TopNav";
 import IconNavBar from "./IconNavBar";
+import { Box } from "@chakra-ui/core";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -11,7 +12,9 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <>
       <TopNav />
       <IconNavBar />
-      {children}
+      <Box p={4} maxW="64rem" mx="auto">
+        {children}
+      </Box>
     </>
   );
 };
