3f0dffc30481144bf11afbc000467a3503e4143d
Kalle <38327916+Sendouc@users.noreply.github.com>
2020-08-06 00:39:21 +0300

    menu disable inactive

--- packages/web-app/components/nav/IconNavBar.tsx
+++ packages/web-app/components/nav/IconNavBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Flex, Image, Link } from "@chakra-ui/core";
+import { Flex, Image } from "@chakra-ui/core";
+import Link from "next/link";
 import useBgColor from "utils/useBgColor";
 
 const icons = [
@@ -17,18 +18,25 @@ const IconNavBar = () => {
   const bg = useBgColor();
   return (
     <Flex bg={bg} justifyContent="center" py={2}>
-      {icons.map(({ name }) => (
-        <Link key={name} href={"/" + name}>
-        <Image
-          src={`/images/navIcons/${name}.webp`}
-          fallbackSrc={`/images/navIcons/${name}.png`}
-          h={12}
-          w={12}
-          mx={2}
-          alt={name}
-        />
-        </Link>
-      ))}
+      {icons.map(({ name }) => {
+        const isDisabled = name !== "plans";
+        return (
+          <Link key={name} href={isDisabled ? "/" : "/" + name}>
+            <Image
+              src={`/images/navIcons/${name}.webp`}
+              fallbackSrc={`/images/navIcons/${name}.png`}
+              h={12}
+              w={12}
+              mx={2}
+              alt={name}
+              cursor={isDisabled ? undefined : "pointer"}
+              style={{
+                filter: isDisabled ? "grayscale(100%)" : undefined,
+              }}
+            />
+          </Link>
+        );
+      })}
     </Flex>
   );
 };
--- packages/web-app/components/nav/TopNav.tsx
+++ packages/web-app/components/nav/TopNav.tsx
@@ -9,13 +9,7 @@ const TopNav = () => {
       <Box color="gray.600" fontWeight="bold" letterSpacing={1}>
         sendou.ink
       </Box>
-      <Image
-        src="https://pbs.twimg.com/profile_images/1274370738452135940/M0omX7PM_400x400.png"
-        borderRadius="50%"
-        w={6}
-        h={6}
-        mr={2}
-      />
+      <Box />
     </Flex>
   );
 };
