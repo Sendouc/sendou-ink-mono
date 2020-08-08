a68c83f9c13594a61b54dd2d30736557a7f19a33
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2020-08-08 14:02:00 +0300

    smaller enemy images

--- packages/web-app/pages/_app.tsx
+++ packages/web-app/pages/_app.tsx
@@ -3,13 +3,11 @@ import theme from "@chakra-ui/theme";
 import { mode, Styles } from "@chakra-ui/theme-tools";
 import Layout from "components/nav/Layout";
 import { AppProps } from "next/app";
-import GoogleFonts from "next-google-fonts";
 
 const styles: Styles = {
   ...theme.styles,
   global: (props) => ({
     ...theme.styles.global,
-    //fontFamily: "body",
     color: mode("blackAlpha.900", "whiteAlpha.900")(props),
     bg: mode("gray.50", "#031e3e")(props),
   }),
@@ -17,11 +15,6 @@ const styles: Styles = {
 
 const customTheme: any = {
   ...theme,
-  /*fonts: {
-    ...theme.fonts,
-    body: `"Rubik",${theme.fonts.body}`,
-    heading: `"Rubik",${theme.fonts.heading}`,
-  },*/
   config: {
     ...theme.config,
     useSystemColorMode: true,
@@ -33,7 +26,6 @@ const customTheme: any = {
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={customTheme}>
-      {/*<GoogleFonts href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap" />*/}
       <CSSReset />
       <Layout>
         <Component {...pageProps} />
